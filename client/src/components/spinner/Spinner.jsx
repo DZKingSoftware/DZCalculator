5aa1b1d3090e521143050e4ddc3e463dfddb2bd9
DZKingSoftware <dilshodzaynitdinov8@gmail.com>
2026-03-11 14:26:44 +0500

    Loader Added V2.1

--- client/src/components/spinner/Spinner.jsx
+++ client/src/components/spinner/Spinner.jsx
@@ -3,7 +3,13 @@ import './spinner.css'
 
 function Spinner() {
     return (
-        <div className="w-full h-screen fixed inset-0 z-99">
+        <div className="w-full h-screen fixed inset-0 flex justify-center items-center"
+            style={{
+                zIndex: '999',
+                backgroundColor: '#00000018',
+                backdropFilter: 'blur(20px)',
+            }}
+        >
             <div className="loader"></div>
         </div>
     )
