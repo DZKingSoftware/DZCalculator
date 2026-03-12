e97a1eba817ed13c8db3b28bac315a439039a3b2
DZKingSoftware <dilshodzaynitdinov8@gmail.com>
2026-03-12 23:20:43 +0500

    Test V4.0

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'https://dz-calculator.netlify.app',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
