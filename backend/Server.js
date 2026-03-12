ecf41b27db15a1d71de4ed7cbb699ad6bc3176e8
DZKingSoftware <dilshodzaynitdinov8@gmail.com>
2026-03-12 23:25:44 +0500

    Test V4.0

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: '*',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
